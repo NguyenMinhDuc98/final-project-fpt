4ab7a414ac7a0547e3a83e2f98867444d5bbf0aa
NguyenMinhDuc98 <ottffssent9897@gmail.com>
2021-04-27 05:26:59 +0700

    fixed

--- src/features/VerifyRepairer/components/listVerifyRepairer.jsx
+++ src/features/VerifyRepairer/components/listVerifyRepairer.jsx
@@ -1,14 +1,24 @@
-import { useDispatch } from "react-redux";
-import { Button, Table } from "reactstrap";
-import { verifyRepairer } from "../verifySlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Spinner } from "reactstrap";
+import { getNotVerifiedList, verifyRepairer } from "../verifySlice";
 import './listVerifyRepairer.scss';
+import ReactFlexyTable from "react-flexy-table"
+import "react-flexy-table/dist/index.css"
+import { useEffect } from "react";
 
 function ListVerifyRepairer() {
+    const verifyList = useSelector(state => state.notVerifiedList);
     const dispatch = useDispatch();
-    const token = localStorage.getItem('token');
-    const nvRepairersList = JSON.parse(localStorage.getItem('verifyList'));
 
-    console.log({ nvRepairersList });
+    const token = localStorage.getItem('token');
+    const nvRepairersList = verifyList.list;
+    const {isLoading} = verifyList;
+
+    console.log({ verifyList });
+
+    useEffect(()=>{
+        dispatch(getNotVerifiedList(token))
+    },[]);
 
     const handleApprove = (id, token) => {
         dispatch(verifyRepairer({
@@ -17,33 +27,63 @@ function ListVerifyRepairer() {
         }));
     };
 
-    const nvRepairers = nvRepairersList.map((nvRepairer) =>
-        <tr key={nvRepairer.id}>
-            <th>{nvRepairer.user.name}</th>
-            <th>{nvRepairer.district}, {nvRepairer.city}</th>
-            {/* <th><img src={nvRepairer.image} alt="logo" /></th> */}
-            <th className="action-col">
-                <Button type='button' onClick={() => handleApprove(nvRepairer.id, token)}>Approve </Button>
-                <Button>Reject</Button>
-            </th>
-        </tr>
-    );
+    const columns = [
+        {
+            header: 'Id',
+            key: 'id',
+            //   td: (data) => <div>the id is {data.id}</div>
+        },
+        {
+            header: 'Username',
+            key: 'user.name',
+            // td: (data) =>
+            //     <div>
+            //         {data.name}
+            //     </div>
+        },
+        {
+            header: 'Phone number',
+            // can also use with nested objects
+            key: 'user.phone_number'
+        },
+        {
+            header: 'Email',
+            key: 'user.email',
+        },
+        {
+            header: 'Action',
+            key: 'action',
+            td: (data) =>
+                <div>
+                    <Button type='button' onClick={() => handleApprove(data.id, token)}>Approve </Button>
+                    <Button>Reject</Button>
+                </div>
+        }
+    ];
 
     return (
-        <div className='nvRepairersList'>
-            <h2>Not verified repairers</h2>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>Repairer</th>
-                        <th>Address</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {nvRepairers}
-                </tbody>
-            </Table>
+        <div>
+            {
+                isLoading
+                    ? <div className='spinner'><Spinner size='xxl' /></div>
+                    : (
+                        <div>
+                            <h3>Not verified repairers</h3>
+                            <ReactFlexyTable
+                                className="verify-table"
+                                data={nvRepairersList}
+                                columns={columns}
+                                sortable
+                                filterable
+                                nonFilterCols={["action"]}
+                                pageSize={10}
+                                pageSizeOptions={[10, 20]}
+                                globalSearch
+                                caseSensitive
+                            />
+                        </div>
+                    )
+            }
         </div>
     );
 }
